378726b3b70543506a8e08ba6e00f0ae24d7b2e3
newfangledCoder <kshitij.raj@hotmail.com>
2020-05-04 22:46:35 +0530

    problem 4 description

---
+++ Challenges2020/May/Problem_4.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace  May
+{
+    public class Problem_4
+    {
+        /*
+            Given a positive integer, output its complement number.
+            The complement strategy is to flip the bits of its binary representation.
+            
+            Example 1:
+
+            Input: 5
+            Output: 2
+            Explanation: The binary representation of 5 is 101 (no leading zero bits),
+            and its complement is 010. So you need to output 2.
+        */
+    }    
+}
